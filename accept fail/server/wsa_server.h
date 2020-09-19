17c03950b4f65a18a671eaa4253ed816d8d6d5fb
ondal1997 <ondal1997@gmail.com>
2020-09-19 18:28:41 +0900

    Done

--- accept fail/server/wsa_server.h
+++ accept fail/server/wsa_server.h
@@ -6,7 +6,7 @@
 #define WM_SOCKET WM_USER+1
 
 extern HWND hWnd;
-SOCKET server_socket;
+SOCKET accept_socket;
 
 BOOL startup_server()
 {
@@ -17,37 +17,37 @@ BOOL startup_server()
 		return FALSE;
 	}
 
-	server_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	accept_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-	if (server_socket == INVALID_SOCKET)
+	if (accept_socket == INVALID_SOCKET)
 	{
 		return FALSE;
 	}
 
 	SOCKADDR_IN server_addr;
 	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	server_addr.sin_port = htons(8000);
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	ZeroMemory(server_addr.sin_zero, sizeof(server_addr.sin_zero));
 
-	if (bind(server_socket, (SOCKADDR*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR)
+	if (bind(accept_socket, (SOCKADDR*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR)
 	{
 		return FALSE;
 	}
 
-	if (listen(server_socket, 1) == SOCKET_ERROR)
+	if (listen(accept_socket, 1) == SOCKET_ERROR)
 	{
 		return FALSE;
 	}
 
-	WSAAsyncSelect(server_socket, hWnd, WM_SOCKET, FD_ACCEPT);
+	WSAAsyncSelect(accept_socket, hWnd, WM_SOCKET, FD_ACCEPT);
 
 	return TRUE;
 }
 
 void cleanup_server()
 {
-	closesocket(server_socket);
+	closesocket(accept_socket);
 
 	WSACleanup();
 }
